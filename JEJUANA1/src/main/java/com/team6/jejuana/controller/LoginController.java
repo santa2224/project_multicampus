9f58045347d116dddf474a766b4ec948f60e9818
superMainV <64447885+santa2224@users.noreply.github.com>
2023-04-14 17:54:24 +0900

    Add files via upload

---
+++ JEJUANA1/src/main/java/com/team6/jejuana/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.team6.jejuana.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+	
+	//로그인 폼 연결	
+	@GetMapping("/login")
+	public ModelAndView login() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login/login");
+		return mav;
+	}
+	
+	//회원가입 폼 연결
+	@GetMapping("/join")
+	public ModelAndView join() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login/join");
+		return mav;
+	}
+}
